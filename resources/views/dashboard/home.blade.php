50e0accca6bab0adb0fdf5799940aaf17cf8406d
rfttra <auliafitra2002@gmail.com>
2024-01-26 16:05:36 +0700

    Authentiaction layout

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title','Homepage')
+
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>Homepage</h1>
+    </div>
+
+    <div class="section-body">
+    </div>
+</section>
+@endsection
+
+@section('sidebar')
+<!-- this is for show the menu in the sidebar menu -->
+@parent
+<li class="menu-header">Starter</li>
+<li class="nav-item dropdown">
+    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+    <ul class="dropdown-menu">
+        <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+        <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+        <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+    </ul>
+</li>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('dashboard.home');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/forgot', function () {
+    return view('auth.forgot');
+})->name('forgot');
+
+Route::get('/reset', function () {
+    return view('auth.reset');
+})->name('reset');
