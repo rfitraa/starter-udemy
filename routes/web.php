51ed59bc649fb4ff06979a40a8b6657c094ae42b
rfttra <auliafitra2002@gmail.com>
2024-01-29 15:42:39 +0700

    User login

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard.home');
+    return view('auth.login');
 });
 
-// Route::get('/login', function () {
-//     return view('auth.login');
-// })->name('login');
-
-// Route::get('/register', function () {
-//     return view('auth.register');
-// })->name('register');
-
-// Route::get('/forgot', function () {
-//     return view('auth.forgot');
-// })->name('forgot');
-
-// Route::get('/reset', function () {
-//     return view('auth.reset');
-// })->name('reset');
+Route::middleware(['auth']) -> group(function(){
+    Route::get('home', function() {
+        // dd(auth()->user());
+        return view('dashboard.home');
+    })->name('home');
+});
